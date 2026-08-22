adafc59fad59a46c59e480a748638c5589cb1bfd
DisccoNektted <79577264+doomer6699@users.noreply.github.com>
2026-08-22 16:55:24 -0600

    Add files via upload

--- useless.sh
+++ useless.sh
@@ -20,6 +20,10 @@
 #command that serves absolutely no purpose.
 #It's not a virus, it's not a useful command, it's just USELESS.
 
+#This has more comments than lines of code
+
+#Hey man, this is licensed under the GNU General Public License, respect it if you're going to use this all-powerful tool...
+
 cat << 'EOF' > useless
 #!/usr/bin/env python3
 print("USELESS COMMAND")
